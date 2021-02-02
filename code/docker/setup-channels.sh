ccb7b147718d57e05fb7bfbea842ba6d8cc9c7a2
Andreas M. Antonopoulos <andreas@antonopoulos.com>
2021-02-02 09:56:01 -0600

    Changing story/example names throughout

--- code/docker/setup-channels.sh
+++ code/docker/setup-channels.sh
@@ -17,13 +17,13 @@ echo Alice to Bob
 docker-compose exec -T Alice lncli -n regtest connect ${bob_address}@Bob
 docker-compose exec -T Alice lncli -n regtest openchannel ${bob_address} 1000000
 
-echo Bob to Wei
-docker-compose exec -T Bob lightning-cli connect ${wei_address}@Wei
+echo Bob to Chan
+docker-compose exec -T Bob lightning-cli connect ${wei_address}@Chan
 docker-compose exec -T Bob lightning-cli fundchannel ${wei_address} 1000000
 
-echo Wei to Gloria
-docker-compose exec -T Wei eclair-cli -p eclair connect --uri=${gloria_address}@Gloria
-docker-compose exec -T Wei eclair-cli -p eclair open --nodeId=${gloria_address} --fundingSatoshis=1000000
+echo Chan to Dina
+docker-compose exec -T Chan eclair-cli -p eclair connect --uri=${gloria_address}@Dina
+docker-compose exec -T Chan eclair-cli -p eclair open --nodeId=${gloria_address} --fundingSatoshis=1000000
 
 echo Get 10k sats invoice from Gloria
 gloria_invoice=$(docker-compose exec -T Gloria bash -c "lncli -n regtest addinvoice 10000 | jq -r .payment_request")
@@ -33,5 +33,5 @@ echo Gloria invoice ${gloria_invoice}
 echo Wait for channel establishment - 60 seconds for 6 blocks
 sleep 60
 
-echo Alice pays Gloria 10k sats, routed around the network
+echo Alice pays Dina 10k sats, routed around the network
 docker-compose exec -T Alice lncli -n regtest payinvoice --json --inflight_updates -f ${gloria_invoice}
